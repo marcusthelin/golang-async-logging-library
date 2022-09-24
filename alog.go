1cddf63f1e68e2e25bd740a7010d0734fd398b96
Marcus Thelin <marcus.thelin@ingka.ikea.com>
2022-09-24 15:56:57 +0200

    Step 8

--- alog.go
+++ alog.go
@@ -32,6 +32,7 @@ func New(w io.Writer) *Alog {
 		dest:    w,
 		msgCh:   make(chan string),
 		errorCh: make(chan error),
+		m: 	 &sync.Mutex{},
 	}
 }
 
@@ -51,10 +52,14 @@ func (al Alog) formatMessage(msg string) string {
 }
 
 func (al Alog) write(msg string, wg *sync.WaitGroup) {
+	al.m.Lock()
+	defer al.m.Unlock()
 	fm := al.formatMessage(msg)
 	_, err := al.dest.Write([]byte(fm))
 	if err != nil {
-		al.errorCh <- err
+		go func() {
+			al.errorCh <- err
+		}()
 	}
 }
 
